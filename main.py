e4579a47faf0256e5e80a28af386f2f3110b44a3
kawaemon <me@kawaemon.dev>
2025-05-15 01:48:14 +0900

    tts class

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from voicevox_core import AccelerationMode
-from voicevox_core.blocking import Onnxruntime,Synthesizer,OpenJtalk,VoiceModelFile
+from voicevox_core.blocking import Onnxruntime, Synthesizer, OpenJtalk, VoiceModelFile
+import subprocess
 
-ZUNDAMON_MODEL=0
-ZUNDAMON_STYLE = 3
+class Tts:
+    ZUNDAMON_MODEL=0
+    ZUNDAMON_STYLE = 3
 
-def main() -> None:
-    onnx = Onnxruntime.load_once(filename="./voicevox_core/onnxruntime/lib/libvoicevox_onnxruntime.1.17.3.dylib")
-    synth = Synthesizer(
-        onnx,
-        OpenJtalk("./voicevox_core/dict/open_jtalk_dic_utf_8-1.11")
-    )
-    with VoiceModelFile.open(f"./voicevox_core/models/vvms/{ZUNDAMON_MODEL}.vvm") as model:
-        synth.load_voice_model(model)
+    def __init__(self) -> None:
+        onnx = Onnxruntime.load_once(filename="./voicevox_core/onnxruntime/lib/libvoicevox_onnxruntime.1.17.3.dylib")
+        synth = Synthesizer(
+            onnx,
+            OpenJtalk("./voicevox_core/dict/open_jtalk_dic_utf_8-1.11")
+        )
+        with VoiceModelFile.open(f"./voicevox_core/models/vvms/{self.ZUNDAMON_MODEL}.vvm") as model:
+            synth.load_voice_model(model)
 
-    wav = synth.tts("こんにちは", ZUNDAMON_STYLE)
-    with open("./output.wav", "wb") as f:
-        f.write(wav)
+        self.synth = synth
 
+    def speak(self, txt: str) -> None:
+        wav = self.synth.tts(txt, self.ZUNDAMON_STYLE)
+        proc = subprocess.Popen(["ffplay", "-autoexit", "-hide_banner", "-"], stdin=subprocess.PIPE)
+        proc.stdin.write(wav)
+        proc.stdin.flush()
+        proc.stdin.close()
+        proc.wait()
+
+
+def main():
+    Tts().speak("こんにちは")
 
 if __name__ == "__main__":
     main()
